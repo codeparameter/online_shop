1a30fded8a426b15dbaab57179486d26557523ea
codeparameter <codeparameter.com@gmail.com>
2025-10-18 00:47:05 +0330

    improving auth decorators

--- project/apps/auth/decorators.py
+++ project/apps/auth/decorators.py
@@ -2,14 +2,22 @@
 
 from functools import wraps
 from flask import jsonify
-from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from project.apps.auth.models import User, UserRole, UserStatus
 
 
-def user_required(*allowed_roles, active_required=False):
-    """Decorator to check if user has required role"""
+def role_required_validation(role, *allowed_roles):
+    return role in allowed_roles
+
+
+def role_restricted_validation(role, *restricted_roles):
+    return role not in restricted_roles
+
+
+def role_based(is_valid_role, *roles, active_required=False):
 
     def decorator(fn):
+        @jwt_required()
         @wraps(fn)
         def wrapper(*args, **kwargs):
             current_user_id = get_jwt_identity()
@@ -18,7 +26,7 @@ def user_required(*allowed_roles, active_required=False):
             if not user:
                 return jsonify({"error": "User not found"}), 404
 
-            if user.role not in allowed_roles:
+            if is_valid_role(user.role, *roles):
                 return jsonify({"error": "Insufficient permissions"}), 403
 
             if active_required and user.status in UserStatus.filtered_list(
@@ -40,11 +48,21 @@ def user_required(*allowed_roles, active_required=False):
     return decorator
 
 
+def role_required(*allowed_roles, active_required=False):
+    return role_based(
+        role_required_validation, *allowed_roles, active_required=active_required
+    )
+
+
+def role_restricted(*restricted_roles, active_required=False):
+    return role_based(
+        role_restricted_validation, *restricted_roles, active_required=active_required
+    )
+
+
 def seller_required(fn, active_required=False):
     """Decorator to check if user is a seller"""
-    return user_required(
-        UserRole.SELLER, UserRole.ADMIN, active_required=active_required
-    )(fn)
+    return user_required(UserRole.SELLER, active_required=active_required)(fn)
 
 
 def admin_required(fn, active_required=False):
